ee508dfe6e91c913d081cd4510840261ea82a540
qazishoukat361-boop <qazishoukat361@gmail.com>
2026-04-09 12:27:09 +0500

    Add Footer component with navigation links

---
+++ Footer.jsx
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom";
+export default function Footer() {
+    return (
+        <footer className="bg-white border-y">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+                <div className="md:flex md:justify-between">
+                    <div className="mb-6 md:mb-0">
+                        <Link to="/" className="flex items-center">
+                            <img
+                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                                className="mr-3 h-16"
+                                alt="Logo"
+                            />
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
+                        <div>
+                            <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
+                            <ul className="text-gray-500 font-medium">
+                                <li className="mb-4  display-block">
+                                    <Link to="/" className="hover:underline">
+                                        Home
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/about" className="hover:underline">
+                                        About
+                                    </Link>
+                                </li>
+
+                              
+
+                            </ul>
+                        </div>
+
+
+                    </div>
+                </div>
+                <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
+                <div className="sm:flex sm:items-center sm:justify-between">
+
+                    <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+
+
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
